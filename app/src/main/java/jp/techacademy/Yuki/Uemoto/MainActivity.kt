27e4cf145cba9fcc09ae2b933910d02ba3682faa
植本 優貴 <y-uemoto@pn2203-032.local>
2022-04-12 10:24:05 +0900

    最初のコミット

---
+++ app/src/main/java/jp/techacademy/Yuki/Uemoto/MainActivity.kt
@@ -0,0 +1,11 @@
+package jp.techacademy.Yuki.Uemoto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
